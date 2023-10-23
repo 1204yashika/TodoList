3761f143a784b8a0766ef78376abbcfbdb8e87c6
1204yashika <1204yashikaagrawal@gmail.com>
2023-10-23 22:36:19 +0530

    new react proj

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const App = ()=>{
+    return (
+        <div className="maindiv">
+            <div className="center_div">
+                <h1>ToDo List</h1>
+                <br/>
+                <input type="text" placeholder="Add a item"/>
+                <button> + </button>
+                <ol>
+                    <li>ahgoa aohgor hoawg</li>
+                </ol>
+            </div>
+        </div>
+    )
+};
+
+export default App;
