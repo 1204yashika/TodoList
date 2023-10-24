2438c991a58c0261dfab9c3fbf780bf799ffc9a9
1204yashika <1204yashikaagrawal@gmail.com>
2023-10-24 19:34:50 +0530

    hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css"
 const App = ()=>{
+    const [inputList, setInputList] = useState("");
+    const [items, setItems]=useState([]);
+    const itemEvent= (event)=>{
+        setInputList(event.target.value);
+    }
+    const listOfItems=()=>{
+        setItems((oldItems)=>{
+            return inputList!==""?[...oldItems,inputList]:[...oldItems];
+        })
+        setInputList("");
+    }
     return (
         <div className="main-div">
             <div className="center-div">
                 <h1>ToDo List</h1>
                 <br/>
-                <input type="text" placeholder="Add a item" onChange={itemEvent}/>
-                <button > + </button>
+                <input type="text" placeholder="Add a item" value={inputList} onChange={itemEvent}/>
+                <button onClick={listOfItems}> + </button>
                 <ol>
-                    <li>ahgoa aohgor hoawg</li>
+                    {
+                        items.map((interval)=>{
+                            return <li>{interval}</li>;
+                        })
+                    }
+                    <li>{inputList}</li>
                 </ol>
             </div>
         </div>
