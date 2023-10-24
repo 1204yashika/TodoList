14e00c2a834cf4d8b672736ca4a947722efd110b
1204yashika <1204yashikaagrawal@gmail.com>
2023-10-24 18:43:38 +0530

    added style

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-
+import "./App.css"
 const App = ()=>{
     return (
-        <div className="maindiv">
-            <div className="center_div">
+        <div className="main-div">
+            <div className="center-div">
                 <h1>ToDo List</h1>
                 <br/>
-                <input type="text" placeholder="Add a item"/>
-                <button> + </button>
+                <input type="text" placeholder="Add a item" onChange={itemEvent}/>
+                <button > + </button>
                 <ol>
                     <li>ahgoa aohgor hoawg</li>
                 </ol>
